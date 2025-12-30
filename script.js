6c58e5fa266bf23de79022ece46ed4255db99f09
Julia Alkhimova <devalchem@gmail.com>
2025-12-30 20:04:09 +0100

    fix(script.js): add Node/Jest export to prevent console ReferenceError

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ const greeting = () => {
     return "Hello World!"
 }
 
-module.exports = greeting;
+
+// Node / Jest export
+if(typeof module !== "undefined" && module.exports){
+    module.exports = greeting;
+}
