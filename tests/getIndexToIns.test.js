aef76480b5c84d11ec6d347aaac46da798494eca
Julia Alkhimova <devalchem@gmail.com>
2025-12-30 20:53:09 +0100

    test(getIndexToIns): add test 2 to check return type

--- tests/getIndexToIns.test.js
+++ tests/getIndexToIns.test.js
@@ -1,7 +1,12 @@
 const { getIndexToIns } = require("../script");
 
 describe("getIndexToIns", () => {
-  test("Should be a function", () => {
+  //  Test 1: You should have a getIndexToIns function.
+  test("should be a function", () => {
     expect(typeof getIndexToIns).toBe("function");
+  });
+  //   Test 2:  getIndexToIns should always return a number.
+  test("should always return a number", () => {
+    expect(typeof getIndexToIns([], 5)).toBe("number");
   });
 });
