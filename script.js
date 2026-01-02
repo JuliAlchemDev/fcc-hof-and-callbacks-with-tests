7f855027217b6d4135fbe78c87cd090b8cbfc242
Julia Alkhimova <devalchem@gmail.com>
2026-01-02 09:46:56 +0100

    chore(script): add user story for first lab

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const greeting = () => {
   return "Hello World!";
 };
 
-// Implement a Sorted Index Finder
+// Lab: Implement a Sorted Index Finder
+
+// User Stories:
+
+// 1. You should have a getIndexToIns function that takes two arguments: 
+// an array and a number.
+// 2. You should use the sort method to sort the array in ascending order.
+// 3. Your getIndexToIns function should return the lowest index 
+// at which the number should be inserted by using the findIndex method.
+// 4. Your getIndexToIns function should always return a number.
 
 const getIndexToIns = (arr, num) => {
   const index = (el) => el >= num;
@@ -23,4 +32,4 @@ if (typeof module !== "undefined" && module.exports) {
   };
 }
 
-// console.log(getIndexToIns([3, 10, 5], 11));
+
