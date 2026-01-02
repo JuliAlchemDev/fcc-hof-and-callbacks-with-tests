dfa305b98d703228f4c34ffefb9d5c0c078f56f8
Julia Alkhimova <devalchem@gmail.com>
2026-01-02 09:35:41 +0100

    feat(script): handle edge case for insertion after max value in getIndexToIns

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const greeting = () => {
-    return "Hello World!"
-}
+  return "Hello World!";
+};
 
 // Implement a Sorted Index Finder
 
-const getIndexToIns  = (arr, num) => {
-   const index = el => el >= num
-    
-  return arr.sort((a,b) => a-b).findIndex(index)
-}
+const getIndexToIns = (arr, num) => {
+  const index = (el) => el >= num;
+  let result = arr.sort((a, b) => a - b).findIndex(index);
+  if(result === -1) {
+    arr.push(num)
+    return arr.sort((a, b) => a - b).findIndex(index);
+  }
+  return result
+};
+
 
 // Node / Jest export
-if(typeof module !== "undefined" && module.exports){
-    module.exports = {
-        greeting,
-        getIndexToIns};
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = {
+    greeting,
+    getIndexToIns,
+  };
 }
 
+// console.log(getIndexToIns([3, 10, 5], 11));
