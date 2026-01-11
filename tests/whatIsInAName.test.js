2d6b8f22d7cb0696cead1d8707d14e42331848a2
Julia Alkhimova <devalchem@gmail.com>
2026-01-11 15:12:12 +0100

    test(whatIsInAName): match all source keys, ignoring extra object properties

--- tests/whatIsInAName.test.js
+++ tests/whatIsInAName.test.js
@@ -31,6 +31,10 @@ describe("whatIsInAName", () => {
   })
   // Test 6: whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat": 2 }], { "apple": 1, "bat": 2 }) 
   // should return [{"apple": 1, "bat": 2}, {"apple": 1, "bat": 2, "cookie": 2}]
+   test("returns objects that match all source keys, ignoring extra object properties", ()=> {
+    expect(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }, { "bat": 2 }], { "apple": 1, "bat": 2 }))
+    .toEqual([{"apple": 1, "bat": 2}, {"apple": 1, "bat": 2, "cookie": 2}])
+  })
   // Test 7: whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3}) 
   // should return [].
   // Test 8: whatIsInAName([{"a": 1, "b": 2, "c": 3, "d": 9999}], {"a": 1, "b": 9999, "c": 3}) 
