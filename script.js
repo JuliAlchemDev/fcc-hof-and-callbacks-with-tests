377a406fc8610200b53603ed18edb6ac4ef71e27
Julia Alkhimova <devalchem@gmail.com>
2026-01-12 09:07:55 +0100

    chore(script): add name and user story for Build a Prime Number Sum Calculator Lab

--- script.js
+++ script.js
@@ -64,6 +64,14 @@ const whatIsInAName = (arr, src) => {
   )
 };
 
+// Build a Prime Number Sum Calculator
+
+// User Stories:
+
+// You should have a sumPrimes function that accepts a number as an argument.
+// The sumPrimes function should return the sum of all prime numbers less than or equal to the provided number.
+// If the input number is less than 2, the function should return 0.
+
 // Node / Jest export
 if (typeof module !== "undefined" && module.exports) {
   module.exports = {
