47e79f5d49e141213533265250ab8ca71725484c
Julia Alkhimova <devalchem@gmail.com>
2026-01-12 09:14:46 +0100

    test(sumPrimes): define expected behavior for sumPrimes function

---
+++ tests/sumPrimes.test.js
@@ -0,0 +1,10 @@
+const { sumPrimes } = require('../script');
+
+describe("sumPrimes", () => {
+//    Test 1: You should have a sumPrimes function.
+// Test 2: sumPrimes(10) should return 17.
+// Test 3: sumPrimes(5) should return 10.
+// Test 4: sumPrimes(2) should return 2.
+// Test 5: sumPrimes(0) should return 0.
+// Test 6: sumPrimes(977) should return 73156.
+})
