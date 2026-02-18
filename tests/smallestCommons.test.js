80eedabeb1f57f2aa7c85cc974fee780c0b1ff99
Julia Alkhimova <devalchem@gmail.com>
2026-02-18 13:44:38 +0100

    test(smallestCommons): check return correct value for [2,10]

--- tests/smallestCommons.test.js
+++ tests/smallestCommons.test.js
@@ -11,7 +11,7 @@ describe("smallestCommons", ()=> {
         expect(typeof smallestCommons([1,5])).toBe("number");
     });
 // Test 3" smallestCommons([1, 5]) should return 60.
-    test("should return a number", ()=> {
+    test("should return 60", ()=> {
         expect(smallestCommons([1,5])).toBe(60);
     });
 // Test 4: smallestCommons([5, 1]) should return 60.
@@ -19,6 +19,9 @@ describe("smallestCommons", ()=> {
         expect(smallestCommons([5,1])).toBe(60);
     });
 // Test 5: smallestCommons([2, 10]) should return 2520.
+ test("should return 2520 for [2,10] case", ()=> {
+        expect(smallestCommons([2,10])).toBe(2520);
+    });
 // Test 6: smallestCommons([1, 13]) should return 360360.
 // Test 7: smallestCommons([23, 18]) should return 6056820.
 })
